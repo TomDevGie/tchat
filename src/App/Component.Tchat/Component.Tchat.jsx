3ffff85038e808458cd1bb60362c1c5c584ed106
thomas.mangin <mangin.thomas@hotmail.fr>
2020-01-08 16:07:45 +0100

    modal react

--- src/App/Component.Tchat/Component.Tchat.jsx
+++ src/App/Component.Tchat/Component.Tchat.jsx
@@ -4,20 +4,21 @@ import Viewer from './Component.Viewer/Component.Viewer';
 import Sender from './Component.Sender/Component.Sender';
 import { createStore } from 'redux'
 import httpGet, { httpPost } from '../fetch';
-
+import Modal from 'react-modal';
+import './Component.Tchat.css';
 
 const messagesReducer = (state = [], action) => {
     switch (action.type) {
       case 'ADD_MESSAGE':
         {
           let obj= [...state,action.message];
-          console.log(obj);
+          // console.log(obj);
           return obj;
         }
       case 'ADD_MESSAGES':
         {
           let obj= [...state,...action.message];
-          console.log(obj);
+          // console.log(obj);
           return obj;
         }
         
@@ -37,7 +38,7 @@ let messageStore = createStore(messagesReducer);
 class Tchat extends Component{
   constructor(props){
     super(props);
-    this.state= {};
+    this.state= {isModalOpen:true};
     httpGet({ressource:'publicDiscussions'},(response)=>
     {
       // messageStore.dispatch({type:'ADD_MESSAGES',message:{text:e.message,userId:1}});
@@ -52,6 +53,15 @@ class Tchat extends Component{
           <div>
             <Viewer store={messageStore} />
              <Sender store={messageStore} />
+             <Modal 
+              isOpen={this.state.isModalOpen}
+              // onAfterOpen={afterOpenModal}
+              // onRequestClose={closeModal}
+              // style={}
+              className="modalStyle"
+              contentLabel="Erreur Modal">
+                  <h2>TEST</h2>
+              </Modal>
           </div>
     
   )
